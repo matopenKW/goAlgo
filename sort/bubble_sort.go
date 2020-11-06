3e711aac356586738e0493474689ec9829cfef59
小野和論 <matopenkw@gmail.com>
2020-11-06 19:17:58 +0900

    Add bubble_sort

---
+++ sort/bubble_sort.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func bubbleSort(numbers []int) []int {
+	for i, _ := range numbers {
+		for j, _ := range numbers[0 : len(numbers)-1-i] {
+			if numbers[j] > numbers[j+1] {
+				numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+			}
+		}
+	}
+	return numbers
+}
+
+func main() {
+	// 0~999までのランダムな数値を生成
+	nums := rand.Perm(1000)[:10]
+
+	fmt.Println(bubbleSort(nums))
+}
